284c83206e6e6973f716e88c1a70b3703178a3e6
Prathamesh2642 <76043914+Prathamesh2642@users.noreply.github.com>
2021-11-19 12:24:12 +0530

    Update assignment10.py

--- assignment10.py
+++ assignment10.py
@@ -15,3 +15,34 @@ for i in range(0,numofstud-1):
 print(perc)
 for i in range(numofstud-1,numofstud-6,-1):
   print(perc[i])
+
+  
+  
+  numofstud=int(input("Enter the number of students "))
+perc=[]
+for i in range(0,numofstud):
+  perc.append(int(input("Enter the percentages of students ")))
+print(perc)
+#bubble sort using while
+i=0
+j=1
+temp=0
+while i<=(2*numofstud-1):
+  if j==numofstud or i==numofstud:
+    if i==numofstud:
+      i=0
+    elif j==numofstud:
+      j=0
+    temp=temp+1
+    if temp==5:
+      break
+  if perc[i]>perc[j]:
+    perc[i],perc[j]=perc[j],perc[i]
+    i=i+1
+    j=j+1
+    print(perc)
+    continue
+  else:
+    i=i+1
+    j=j+1
+    print(perc)
